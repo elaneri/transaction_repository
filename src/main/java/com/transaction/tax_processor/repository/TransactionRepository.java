f028bd74da38265dd4944a1067e0cc2975e2b7a4
elaneri <elaneri@gmail.com>
2025-02-10 09:40:03 -0300

    adding batch get

---
+++ src/main/java/com/transaction/tax_processor/repository/TransactionRepository.java
@@ -0,0 +1,12 @@
+package com.transaction.tax_processor.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.transaction.tax_processor.entity.Transaction;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+	List<Transaction> findByBatchTransactionId(String batchTransactionId);
+}
