9d3407278509e028f3cd5f8a38c4277587921d46
elaneri <elaneri@gmail.com>
2025-02-10 21:26:28 -0300

    adding taxes endpoints

--- src/main/java/com/transaction/tax_processor/repository/TransactionRepository.java
+++ src/main/java/com/transaction/tax_processor/repository/TransactionRepository.java
@@ -2,11 +2,16 @@ package com.transaction.tax_processor.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.transaction.tax_processor.entity.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-	List<Transaction> findByBatchTransactionId(String batchTransactionId);
+	List<Transaction> findByBatchTransactionId(String batchTransactionId, Pageable pageable);
+
+	Transaction findByTransactionId(String transactionId);
+
+
 }
